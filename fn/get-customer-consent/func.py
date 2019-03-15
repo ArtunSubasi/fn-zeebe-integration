c0b582d7ff03239d5825d70d9e2143244f35c92d
Artun Subasi <artun.subasi@esentri.com>
2019-03-15 15:19:54 +0100

    Implement the get-customer-consent function

--- fn/get-customer-consent/func.py
+++ fn/get-customer-consent/func.py
@@ -5,14 +5,14 @@ from fdk import response
 
 
 def handler(ctx, data: io.BytesIO=None):
-    customer_number = 0
+    notification_permitted = False
     try:
         body = json.loads(data.getvalue())
         customer_number = body.get("customerNumber")
+        notification_permitted = customer_number % 2 == 1
     except (Exception, ValueError) as ex:
         print(str(ex))
 
-    notification_permitted = customer_number % 2 == 1
     return response.Response(
         ctx, response_data = json.dumps(
             {"notificationPermitted": notification_permitted}),
