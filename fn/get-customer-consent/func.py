e0852c47cbc0e0cdc568255af2ebc53a7da808ac
Artun Subasi <artun.subasi@esentri.com>
2019-03-30 14:21:16 +0100

    Update the customer-consent function

--- fn/get-customer-consent/func.py
+++ fn/get-customer-consent/func.py
@@ -4,12 +4,18 @@ import json
 from fdk import response
 
 def handler(ctx, data: io.BytesIO=None):
-    body = json.loads(data.getvalue())
-    customer_number = body.get("customerNumber")
-    notification_permitted = customer_number % 2 == 1
-    
+    try:
+        notification_permitted = is_notification_permitted(data.getvalue())
+    except (Exception) as ex:
+        notification_permitted = False
+        
     return response.Response(
         ctx, response_data = json.dumps(
             {"notificationPermitted": notification_permitted}),
         headers={"Content-Type": "application/json"}
     )
+
+def is_notification_permitted(request_body):
+    customer = json.loads(request_body)
+    customer_number = customer.get("customerNumber")
+    return customer_number % 2 == 1
